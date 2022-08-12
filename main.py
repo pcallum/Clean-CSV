392f1e25d4ed3691b858262c4f542f9f4129ea5d
pcallum <callumrp10@gmail.com>
2022-08-12 16:08:57 +0100

    added file writing

--- main.py
+++ main.py
@@ -1,19 +1,15 @@
 import csv
 
-with open('imdb_data.csv', encoding="utf8") as csv_file:
+with open('imdb_data.csv', 'r', encoding="utf8") as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
-    line_count = 0
-    empty_entries = 0
+    clean_file = []
+
     for row in csv_reader:
-        for entry in row:
-            if entry == "":
-                print("None" + ",", end=' ')
-                empty_entries += 1
-            else:
-                print(entry + ",", end=' ')
+        # appends the cleaned row with empty entries replaced and whitespace stripped to a clean file
+        clean_file.append([("None" if (x == "") else x.rstrip()) for x in row])
 
-        print("\n")
 
-        line_count += 1
-    print(str(line_count) + " lines.")
-    print(str(empty_entries) + " empty entries.")
+with open('imdb_data_clean.csv', 'w', encoding="utf8") as csv_file:
+    # write clean file to a new file
+    csv_writer = csv.writer(csv_file)
+    csv_writer.writerows(clean_file)
