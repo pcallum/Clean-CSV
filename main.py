b389843e47b982dcb2af0e203537d0bb6efa0728
pcallum <callumrp10@gmail.com>
2022-08-16 20:46:56 +0100

    script now deletes rows with empty entries

--- main.py
+++ main.py
@@ -1,35 +1,26 @@
 import csv
 import logging
 
-logging.basicConfig(filename = 'imdb_csv_log.log',
-                    level = logging.DEBUG,
-                    format = '%(asctime)s:%(levelname)s:%(name)s:%(message)s')
+logging.basicConfig(filename='imdb_csv_log.log',
+                    level=logging.DEBUG,
+                    format='%(asctime)s:%(levelname)s:%(name)s:%(message)s')
 
-with open('imdb_data.csv', 'r', encoding="utf8") as csv_file:
-    logging.info("imdb_data.csv opened in reading mode.")
+deleted_rows = 0
 
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    clean_file = []
-    clean_row = []
-    empty_entries_in_file = 0
+with open('imdb_data.csv', 'r', encoding="utf8") as old_file, open('imdb_data_clean.csv', 'w', encoding="utf8",
+                                                                   newline='') as new_file:
+    logging.info("Script started.")
+
+    csv_reader = csv.reader(old_file, delimiter=',')
+    csv_writer = csv.writer(new_file)
+
     for row in csv_reader:
-        for entry in row:
-            if entry == "":
-                clean_row.append("None")
-                empty_entries_in_file += 1
-            else:
-                clean_row.append(entry.rstrip())
+        if "" in row:
+            deleted_rows += 1
+        else:
+            csv_writer.writerow([entry.rstrip() for entry in row])
 
-        clean_file.append(clean_row)
-        clean_row = []
+    if deleted_rows > 0:
+        logging.warning(f"Deleted {deleted_rows} rows with empty entries.")
 
-    logging.warning(f"File has {empty_entries_in_file} empty entries.")
-
-logging.info("imdb_data.csv closed.")
-
-with open('imdb_data_clean.csv', 'w', encoding="utf8", newline='') as csv_file:
-    logging.info("imdb_data_clean.csv opened.")
-    # write clean file to a new file
-    csv_writer = csv.writer(csv_file)
-    csv_writer.writerows(clean_file)
-logging.info("imdb_data_clean.csv closed.")
+    logging.info("Script finished.")
